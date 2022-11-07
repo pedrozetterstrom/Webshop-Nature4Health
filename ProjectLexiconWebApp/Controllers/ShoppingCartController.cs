5d2ba5851e2cb62605f9914832e422523ed33902
Daniel Oikarainen <powerdeveloper@fastmail.se>
2022-11-07 13:34:14 +0100

    #51 implemented

--- ProjectLexiconWebApp/Controllers/ShoppingCartController.cs
+++ ProjectLexiconWebApp/Controllers/ShoppingCartController.cs
@@ -7,6 +7,7 @@ using System.Text;
 using Microsoft.AspNetCore.Http;
 using ProjectLexiconWebApp.ViewModels;
 using System.Security.Policy;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace ProjectLexiconWebApp.Controllers
 {
@@ -164,6 +165,107 @@ namespace ProjectLexiconWebApp.Controllers
             HttpContext.Session.SetString("CurrentCustomerCart", "");
 
             return RedirectToAction("Index");
+        }
+
+        //Consideration
+        //Validation
+        //1. Enough $$$ of customer
+        //2. Enough products in stock?
+        //3. Customer logged in
+        //4. 
+        //[HttpPost]
+        public async Task<IActionResult> PlaceAnOrder()
+        {            
+            string currentSessionString = HttpContext.Session.GetString("CurrentCustomerCart");
+            decimal totalCost = await GetTotalCost();
+
+            //Hard code a customer - Pedro
+            int currentCustomerId = 1;
+            //Get Customer
+            Customer currentCustomer = await _context.Customers.FirstOrDefaultAsync(aCustomer => aCustomer.Id == currentCustomerId);
+            decimal customerMoneyAmount = currentCustomer.Wallet;
+
+            //Customer enough $$$
+            if (customerMoneyAmount < totalCost)
+                return View("Denied");            
+
+
+            //ProductId -> CountOfProducts (aka cart)
+            Dictionary<int, int> cartDictionary = CreateCartDicFromSessionData();
+
+            //Create an order
+            Order newOrder = new Order
+            {
+                //TotalCost = totalCost,
+                OrderDate = DateTime.Now,
+                Status = "pending",
+                CustomerId = currentCustomer.Id,
+                ShipperId = null
+            };
+            _context.Orders.Add(newOrder);
+            _context.SaveChanges();
+
+            foreach (var item in cartDictionary)
+            {
+                int currentProductIdInCart = item.Key;         //Aka product ID
+                int currentCountProductInCart = item.Value;     //Count products of that ID
+
+                Product myProduct = _context.Products.FirstOrDefault(product => product.Id == currentProductIdInCart);
+
+                //Not enough product(s) in stock
+                if (currentCountProductInCart > myProduct.Quantity) 
+                {
+                    return View("Denied");
+                }
+                else
+                {
+                    //remove currentCountProductInCart cartItem(s) from db products
+                    //Update product database database(s) here
+                    //Lager saldo
+                    myProduct.Quantity -= currentCountProductInCart;
+                    _context.Update(myProduct);
+                    _context.SaveChanges();
+
+                    OrderItem newOrderItem = new OrderItem 
+                    {
+                        OrderId = newOrder.Id,
+                        ProductId = myProduct.Id,
+                        Quantity = currentCountProductInCart
+                    };
+                    _context.OrderItems.Add(newOrderItem);
+                    _context.SaveChanges();
+
+                    //remove customer $$$ wallet with (myProduct.UnitPrice*currentCountProductInCart) amount
+                    //currentCustomer.Wallet -= (myProduct.UnitPrice * currentCountProductInCart);
+                }
+            }
+
+            //and save new state of currentCustomer to db
+            currentCustomer.Wallet -= totalCost;
+            _context.Update(currentCustomer);
+            _context.SaveChanges();
+            
+            return View("Passed");
+        }
+
+        //Calculate total cost based on session string "CurrentCustomerCart"
+        public async Task<decimal> GetTotalCost() 
+        {
+            decimal totalCost = 0;
+            Dictionary<int, int> cartDictionary = CreateCartDicFromSessionData();
+
+            foreach (var item in cartDictionary)
+            {
+                int currentProductIdInCart = item.Key;         //Aka product ID
+                int currentCountProductInCart = item.Value;     //Count products of that ID
+
+                Product myProduct = await _context.Products.FirstOrDefaultAsync(product => product.Id == currentProductIdInCart);
+
+                totalCost += (currentCountProductInCart * myProduct.UnitPrice);
+            }
+
+            return totalCost;
+
         }
     }
 }
